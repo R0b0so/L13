5e3d50627199fecbc64da77457c264c4994363db
R0b0so <ethansagar222@gmail.com>
2025-01-12 17:06:48 -0800

    hello

---
+++ calculator.py
@@ -0,0 +1,26 @@
+
+def add(num1, num2):
+    addition = num1 + num2
+    return addition
+
+
+def sub(num1, num2):
+    subtraction = num1 - num2
+    return subtraction
+
+
+def mult(num1, num2):
+    multiply = num1 * num2
+    return multiply
+
+
+def div(num1, num2):
+    divide = num1 / num2
+    return divide
+
+
+print("d, div")
+print("c, mult")
+print("b, sub")
+print("a, add")
+choice = input("please make your choice a\ b\ c\ d\ ")
---
+++ homework.py
@@ -0,0 +1,4 @@
+r = int(input("What is the radius of the circle?"))
+d = r + r
+c = d * 3.14
+print("The diameter is ", d, " and the circumfrence is ", c)
